d4c619da5e27f8c0411ba0262a07de873a458b63
Eliseo Gomez <jose.eliseo.gomez@gmail.com>
2022-07-28 08:24:17 -0600

    base documents

---
+++ Example/build/Pods.build/Debug-iphoneos/Pods-nRFMeshProvision_Example.build/DerivedSources/Pods_nRFMeshProvision_Example_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_nRFMeshProvision_ExampleVersionString[];
+ extern const double Pods_nRFMeshProvision_ExampleVersionNumber;
+
+ const unsigned char Pods_nRFMeshProvision_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_nRFMeshProvision_Example  PROJECT:Pods-1" "\n";
+ const double Pods_nRFMeshProvision_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Example/build/Pods.build/Debug-iphoneos/nRFMeshProvision.build/DerivedSources/nRFMeshProvision_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char nRFMeshProvisionVersionString[];
+ extern const double nRFMeshProvisionVersionNumber;
+
+ const unsigned char nRFMeshProvisionVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:nRFMeshProvision  PROJECT:Pods-1" "\n";
+ const double nRFMeshProvisionVersionNumber __attribute__ ((used)) = (double)1.;
